b93b89888619504ec917ea566c0dc11ee8a25d62
Plinio Mabesi <pliniomabesi@gmail.com>
2023-11-12 11:14:23 -0300

    Deploy and upgrade configurations

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,8 +2,23 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  networks: {
+    mumbai: {
+      url: process.env.RPC_URL,
+      chainId: parseInt(`${process.env.CHAIN_ID}`),
+      accounts: {
+        mnemonic: process.env.SECRET,
+      }
+    }
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY
+  }
 };
 
 export default config;
--- scripts/update.ts
+++ scripts/update.ts
@@ -3,12 +3,12 @@ import { ethers, upgrades } from "hardhat";
 async function main() {
 
   const OZMultiToken = await ethers.getContractFactory("OZMultiToken");
-  const cc = await upgrades.deployProxy(OZMultiToken);
+  const cc = await upgrades.upgradeProxy("0x0b9a65cC2Bb4b4c25834C6E943478beB1D9dA590", OZMultiToken);
 
   await cc.waitForDeployment();
   const address = await cc.getAddress();
 
-  console.log(`Contract OZMultiToken Proxy deployed to ${address}`);
+  console.log(`Contract OZMultiToken Proxy updated to ${address}`);
 
 }
 
